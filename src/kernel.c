6a6cc19962a7830ecc5686bf9e8c8d98d8ca9537
voukatas <sttelioss@gmail.com>
2024-09-09 02:11:02 +0300

    fixed linker.ld, improved print string func

--- src/kernel.c
+++ src/kernel.c
@@ -2,22 +2,47 @@
 
 static void print_char(char c, uint8_t color) {
   static uint16_t *video = (uint16_t *)VIDEO;
+  static uint16_t row = 0;
+  static uint16_t column = 0;
+  if (c == '\n') {
+    uint16_t offset = (VGA_WIDTH - column);
+    video = video + offset;
+    return;
+  }
   *video = (color << 8) | c;
   video++;
+  column++;
+  if (column == VGA_WIDTH) {
+    column = 0;
+    row++;
+  }
+}
+
+size_t strlen(const char *s) {
+  size_t len = 0;
+  while ((*s++)) {
+    len++;
+  }
+
+  return len;
 }
 
 void print_string(char *s, uint8_t color) {
-  char c;
-  while ((c = *s++) != '\0') {
-    print_char(c, color);
+  size_t len = strlen(s);
+  for (size_t i = 0; i < len; i++) {
+    print_char(s[i], color);
   }
+  // char c;
+  // while ((c = *s++) != '\0') {
+  //   print_char(c, color);
+  // }
 }
 
 void clear_screen() {
   uint16_t *video = (uint16_t *)VIDEO;
-  for (int i = 0; i < 25; i++) {
-    for (int j = 0; j < 80; j++) {
-      uint16_t offset = i * 80 + j;
+  for (size_t i = 0; i < VGA_HEIGHT; i++) {
+    for (size_t j = 0; j < VGA_WIDTH; j++) {
+      uint16_t offset = i * VGA_WIDTH + j;
       *(video + offset) = (0 << 8) | 'A';
     }
   }
@@ -25,7 +50,7 @@ void clear_screen() {
 
 void kernel_main(void) {
   clear_screen();
-  print_string("Hello World!\0", 0x02);
-  print_string(" \0", 0x02);
-  print_string("Hello Kernel!\0", 0x02);
+  print_string("Hello World!\0", 15);
+  print_string("\n\0", 15);
+  print_string("Hello Kernel!\0", 15);
 }
--- src/kernel.h
+++ src/kernel.h
@@ -2,11 +2,16 @@
 #define KERNEL_H
 
 #include <stdint.h>
+#include <stddef.h>
 
 #define VIDEO 0xB8000
 
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
+
 void kernel_main(void);
-void print_string(char *s, uint8_t color);
+//void print_string(char *s, uint8_t color);
+size_t strlen(const char *s);
 
 
 #endif
